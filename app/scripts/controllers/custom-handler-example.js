e827455de6c123d5f871cf829e251aa075dd5ec9
abramz <andrew.shapro@gmail.com>
2013-10-14 13:37:41 -0700

    fixed examples for latest hover-spy build

--- app/scripts/controllers/custom-handler-example.js
+++ app/scripts/controllers/custom-handler-example.js
@@ -6,19 +6,19 @@ angular.module('angular-hover-spy-example')
     '$scope',
     function ($rootScope, $scope) {
       $scope.message = 'I mirror the other elements hover effects!';
-      $rootScope.$on('abramz.custom-handler:hover-spy-mouseenter', function () {
+      $rootScope.$on('custom-handler:abramz.hover-spy-mouseenter', function () {
         $scope.message = 'You have to be really fast to see this!!';
       });
 
-      $rootScope.$on('abramz.custom-handler:hover-spy-mouseenter-handled', function () {
+      $rootScope.$on('custom-handler:abramz.hover-spy-mouseenter-handled', function () {
         $scope.message = 'See how this message changed when you hovered over the other element?';
       });
 
-      $rootScope.$on('abramz.custom-handler:hover-spy-mouseleave', function () {
+      $rootScope.$on('custom-handler:abramz.hover-spy-mouseleave', function () {
         $scope.message = 'You have to be really fast to see this!!';
       });
 
-      $rootScope.$on('abramz.custom-handler:hover-spy-mouseleave-handled', function () {
+      $rootScope.$on('custom-handler:abramz.hover-spy-mouseleave-handled', function () {
         $scope.message = 'I mirror the other elements hover effects!';
       });
 
